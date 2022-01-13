ef7e68be4b69be05609df880be48189257aaab7b
cloyan <cloyan@ebay.com>
2022-01-13 04:16:18 +0300

    morgan package included

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const morgan = require('morgan')    // Middleware'de kullandığımız, raporlama işlemi yapan pakettir.
 
 const app = express()
 app.set('view engine', 'ejs')
 
 app.listen(3000)
+
+app.use((req, res, next) => {   // Middleware
+    console.log(`    Req method: ${req.method} 
+    Req path: ${req.path} 
+    Res Status: ${res.statusCode} 
+    Middleware çalıştı...`);
+    next();  // Middleware çalıştıktan sonra next() ile sonraki adıma devam etmesini söyledik.
+})
+// next() ile bir sonraki adıma devam edildiği için aşağıdaki istekler çalışabildi.
+
+app.use(morgan('dev')) 
 
 app.get('/', (req, res) => {
     res.render('index', {title: 'Anasayfa'})
