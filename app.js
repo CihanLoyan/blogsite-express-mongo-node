b93a6513201a96a7af2b6003ba9c7353a27ad6e0
cloyan <cloyan@ebay.com>
2022-01-13 03:03:39 +0300

    view engine ejs added and changed sendfile instead of render

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ app.set('view engine', 'ejs')
 app.listen(3000)
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname})
+    res.render('index')
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname})
+    res.render('about')
 })
 
 app.get('/about-us', (req, res) => {
@@ -18,5 +18,5 @@ app.get('/about-us', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname})
+    res.status(404).render('404')
 })
