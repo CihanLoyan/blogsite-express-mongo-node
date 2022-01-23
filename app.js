07335986307f75cfdaae25733791d57e18a61e90
cloyan <cloyan@ebay.com>
2022-01-23 13:09:26 +0300

    mongoDB connected and routing to pages actions

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const morgan = require('morgan')    // Middleware'de kullandığımız, raporlama işlemi yapan pakettir.
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const adminRoutes = require('./routes/adminRoutes')
+const blogRoutes = require('./routes/blogRoutes')
+const authRoutes = require('./routes/authRoutes')
+const { requireAuth, checkUser } = require('./middlewares/authMiddlewares')
 
 const app = express()
-app.set('view engine', 'ejs')
 
-app.listen(3000)
+const dbURL = 'mongodb+srv://cihan-234:cihan-234@nodeblog.kwjns.mongodb.net/express-blog?retryWrites=true&w=majority'
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err))
+
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(morgan('dev'))
+app.use(cookieParser())
+
 
 app.use((req, res, next) => {   // Middleware
     console.log(`    Req method: ${req.method} 
@@ -15,20 +31,22 @@ app.use((req, res, next) => {   // Middleware
 })
 // next() ile bir sonraki adıma devam edildiği için aşağıdaki istekler çalışabildi.
 
-app.use(morgan('dev')) 
-
-app.get('/', (req, res) => {
-    res.render('index', {title: 'Anasayfa'})
-})
+app.use('*', checkUser)
+app.get('/', (req, res) => { res.redirect('/blog') })
+app.use('/', authRoutes)
+app.use('/blog', blogRoutes)
+app.use('/admin', requireAuth, adminRoutes)
 
 app.get('/about', (req, res) => {
-    res.render('about', {title: 'Hakkımızda'})
+    res.render('about', { title: 'Hakkımızda' })
 })
 
 app.get('/about-us', (req, res) => {
     res.redirect('/about')
 })
 
+
 app.get('*', (req, res) => {
-    res.status(404).render('404', {title: `${res.statusCode} Not Found !`})
+    res.status(404).render('404', { title: `${res.statusCode} Not Found !` })
 })
+
