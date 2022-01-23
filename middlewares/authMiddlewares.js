7d7033d30635e908dfc65f2a2232d21c389175cf
cloyan <cloyan@ebay.com>
2022-01-23 13:25:25 +0300

    authorization with json web tokens

---
+++ middlewares/authMiddlewares.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/users');
+
+const requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt
+
+    if (token) {
+        jwt.verify(token, 'gizli kelime', (err, decodedToken) => {
+            if (err) {
+                console.log(err);
+                res.redirect('/login')
+            } else {
+                console.log(decodedToken);
+                next()
+            }
+        })
+    } else {
+        res.redirect('/login')
+    }
+}
+
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt
+
+    if (token) {
+        jwt.verify(token, 'gizli kelime', async (err, decodedToken) => {
+            if (err) {
+                console.log(err);
+                res.locals.user = null
+            } else {
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id)
+                res.locals.user = user
+                next()
+            }
+        })
+    } else {
+        res.locals.user = null
+        next()
+    }
+}
+
+module.exports = { requireAuth, checkUser }
