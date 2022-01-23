0b9f48d5cde45747f507edbe65afadb9012729e9
cloyan <cloyan@ebay.com>
2022-01-23 13:19:08 +0300

    schema created for blog

---
+++ models/blogs.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        require: true,
+    },
+    short: {
+        type: String,
+        require:true,
+    },
+    long: {
+        type: String,
+        require: true,
+    },
+}, {timestamps: true})
+
+const Blog = mongoose.model('Blog', blogSchema)
+module.exports = Blog
